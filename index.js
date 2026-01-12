c783ad646420e7ec317fe5fc8ab2bbd4d56bd20b
ArmandoValerio <armandovaleriosalmeron@gmail.com>
2026-01-11 21:32:16 -0600

    Pipeline 1.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 // Registro simple de todas las solicitudes (ayuda a depurar)
 app.use((req, res, next) => {
